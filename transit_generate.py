20c1c48a70b6df62e60dfc5c0699284d78077fe0
Bushwallyta271828 <bushwallyta@gmail.com>
2015-08-28 11:46:09 +0100

    Reduced redundant code.

--- transit_generate.py
+++ transit_generate.py
@@ -37,29 +37,11 @@ def generate():
     inpt = []
     outp = []
     for o in range(5):
+        O = normal(1000, 100, (1000,))
+        O[O < 300] = 300
         if uniform() < 0.5:
-            O = normal(1000, 100, (1000,))
-            O[O < 300] = 300
-            epsilon = normal(0, 200, (1000,))
-            epsilon[-epsilon > O] = 0
-            I = alpha * (O + epsilon + beta)
-            I /= average(I)
-            partitioning = compartmentalize(I)[0]
-            for num, point in enumerate(partitioning[:-1]):
-                next_point = partitioning[num + 1]
-                if (next_point - 1) // 100 > (point - 1) // 100:
-                    block = I[point: next_point]
-                    maximum, minimum = max(block), min(block)
-                    if point % 100 > 0:
-                        start = 100 * (point // 100 + 1)
-                    else:  # point % 100 == 0
-                        start = point
-                    for i in range(start, next_point, 100):
-                        inpt += [maximum, minimum]
             outp += [False]
         else:
-            O = normal(1000, 100, (1000,))
-            O[O < 300] = 300
             center = randint(0, 1000)
             spread = randint(50, 500)
             start = center - spread
@@ -77,21 +59,21 @@ def generate():
                 if i in span:
                     O[i] *= 1 - 0.33*(1/pi * arccos((10 - x) / 10)
                          - (1 / (2*pi)) * (10 - x) * sqrt((20 - x) * x))
-            epsilon = normal(0, 200, (1000,))
-            epsilon[-epsilon > O] = 0
-            I = alpha * (O + epsilon + beta)
-            I /= average(I)
-            partitioning = compartmentalize(I)[0]
-            for num, point in enumerate(partitioning[:-1]):
-                next_point = partitioning[num + 1]
-                if (next_point - 1) // 100 > (point - 1) // 100:
-                    block = I[point: next_point]
-                    maximum, minimum = max(block), min(block)
-                    if point % 100 > 0:
-                        start = 100 * (point // 100 + 1)
-                    else:  # point % 100 == 0
-                        start = point
-                    for i in range(start, next_point, 100):
-                        inpt += [maximum, minimum]
             outp += [True]
+        epsilon = normal(0, 200, (1000,))
+        epsilon[-epsilon > O] = 0
+        I = alpha * (O + epsilon + beta)
+        I /= average(I)
+        partitioning = compartmentalize(I)[0]
+        for num, point in enumerate(partitioning[:-1]):
+            next_point = partitioning[num + 1]
+            if (next_point - 1) // 100 > (point - 1) // 100:
+                block = I[point: next_point]
+                maximum, minimum = max(block), min(block)
+                if point % 100 > 0:
+                    start = 100 * (point // 100 + 1)
+                else:  # point % 100 == 0
+                    start = point
+                for i in range(start, next_point, 100):
+                    inpt += [maximum, minimum]
     return (inpt, outp)
