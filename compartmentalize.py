f9ba2dfb4ca75ae77db363bfa402b0adb4ef2fa1
Bushwallyta271828 <bushwallyta@gmail.com>
2015-08-29 15:07:59 +0100

    updated compartmentalize to Dynamic v2.0 standards.

--- compartmentalize.py
+++ compartmentalize.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
 from pylab import *
-from matplotlib import cm
-from scipy.stats import norm
-from astropy.io import fits
+from numpy import *
 
 """
 Comments:
@@ -12,21 +10,14 @@ heights.txt must be kept in the same directory at all times!
 """
 
 def compartmentalize(lightcurve,
-                     x=0,
-                     memo=None,
                      max_length=float("inf"),
-                     barmap=None,
-                     max_pval=0.2,
-                     min_size=None):
+                     max_pval=0.2):
     """
     Author: Xander
     compartmentalize(...) is the backbone of the Dynamic Bins program.
-    Given a lightcurve, a starting frame number, a memo table, the maximum
-    number of frames that can be put in a bin, list of (index=number
-    of items in a bin) to (value_at_index=expectation value of maximum -
-    minimum given a standard deviation of 1), the maximum fraction of the time
-    a measurement may lie outside the error bars, and the minimum number of 
-    frames a bin can contain, this function returns a tuple containing:
+    Given a lightcurve, the maximum number of frames that can be put in a bin,
+    and the maximum fraction of the time a measurement may lie
+    outside the error bars, this function returns a tuple containing:
         1.) A list of the optimal breaking points.
             In other words, if the first item in the returned tuple reads
             [a, b, c, ..., z], then the best bins to use are [a, b), [b, c),
@@ -45,8 +36,8 @@ def compartmentalize(lightcurve,
             divided by part of the average and just make our problem the minimization
             of the sum of the standard deviations, weighted by size, of the bins. 
             However, since the end result only depends on the error bar heights,
-            and not the standard deviations themselves, we can use barmap to
-            infer the standard deviation (assuming the lightcurve is distributed
+            and not the standard deviations themselves, we can use zeta in heights.txt
+            to infer the standard deviation (assuming the lightcurve is distributed
             as a gaussian for small intervals) with the formula:
                 standard_deviation = height / barmap[number of frames]
             Since we must weigh the bins proportionally with the number of frames
@@ -54,15 +45,8 @@ def compartmentalize(lightcurve,
                 badness of a bin = (number of frames) * height / barmap[number of frames]
             The returned partitioning is the partitioning, given that 
             no bin can have more than max_length items in it, that minimizes the total 
-            badness.
-        3.) The new memo table. This third returned item is only helpful to further
-            recursive calls of the compartmentalize function - it is of no direct use to
-            the user.
-    Default values mean that the user only HAS to input a lightcurve. If not set,
-    barmap will be pulled heights.txt, which has this exact list written out as a 
-    .txt file for reasonable bin sizes (starting with 2). The only reasonable optional
-    keywords for a user to specify are max_length and max_pval.
-    The program uses Dynamic Programming to find the best partitioning in O(n) time,
+            badness. For more information on the algorithm, check out Dynamic Writeup.pdf
+    The function uses Dynamic Programming to find the best partitioning in O(n) time,
     where n is the length of the lightcurve. Since any algorithm must at least look
     at every intensity of the lightcurve, this program provides the best
     concieveable big-O. However, the constant is quite large.
@@ -70,78 +54,43 @@ def compartmentalize(lightcurve,
     requirement, which saves us from looking at blocks stretching very long
     distances. If max_length is set to float("inf"), for instance, the algorithm
     becomes O(n^2). Think of the program as O(n * min(n, max_length - min_size)).
-    The last two keyword arguments passed in are linked. The user specifies max_pval,
-    the maximum probability that a measurement can land outside the error bars, which
-    the computer then uses to find min_size, the minimum number of frames allowed in
-    a bin. Since there is no need to re-compute this quantity, it is passed in directly
-    to recursive calls. The formula for min_size in terms of max_pval is:
-        min_size = max(2, int(2 / max_pval))
-    This can be derived by some combination of math and quick computer scripts to 
-    check that math for small values of min_size.
+    Older versions of this method are implemented with a recursive algorithm.
+    This function is now implemented iteratively, making it somewhat faster (~0.75 
+    seconds for the old program becomes ~0.6 seconds for the newer version due to
+    iterative methods alone - further speedups may have ensued due to better programming
+    in other ways).
     """
-    if memo == None: memo = {}
-    if barmap == None:
-        max_length = min(max_length, len(lightcurve))
-        f = open("heights.txt")
-        lines = f.readlines()
-        f.close()
-        barmap = [None, None] #Neither 0 nor 1 may be called - they have no meaning.
-        for line in lines:
-            height = float(line[:-1].split(":")[1])
-            barmap.append(height)
-        slope = (barmap[-1] - barmap[-21]) / 20
-        while max_length > len(barmap) - 1:
-            barmap.append(barmap[-1] + slope)
-    if min_size == None:
-        min_size = max(2, int(2 / max_pval))
-        """
-        Inverting the formula pval = 2 / (min_size + 1), but since int
-        rounds down, the -1 must be discarded.
-        """
-    if x in memo: return memo[x] + (memo,)
-    elif len(lightcurve) - x < min_size:
-        partition = [x, len(lightcurve)]
-        badness = float("inf")
-        return (partition, badness, memo)
-    elif len(lightcurve) - x in [2, 3]:
-        partition = [x, len(lightcurve)]
-        block = lightcurve[x:]
-        height = max(block) - min(block)
-        badness = len(block) * height / barmap[len(block)]
-        return (partition, badness, memo)
-    else:
-        pre_lightcurve_slice = lightcurve[x: x + min_size]
-        maximum = max(pre_lightcurve_slice)
-        minimum = min(pre_lightcurve_slice)
+    l = len(lightcurve)
+    max_length = min(max_length, l)
+    f = open("heights.txt")
+    lines = f.readlines()
+    f.close()
+    barmap = [None, None]
+    for line in lines:
+        height = float(line[:-1].split(":")[1])
+        barmap.append(height)
+    slope = (barmap[-1] - barmap[-21]) / 20
+    while max_length > len(barmap) - 1:
+        barmap.append(barmap[-1] + slope)
+    min_size = max(2, int(2 / max_pval))
+    memovalues = [None]*l + [([l], 0)]
+    memovalues[l - min_size + 1:-1] = [([], float("inf"))]*(min_size - 1)
+    i = l - min_size
+    while i >= 0:
+        minimum = min(lightcurve[i:i + min_size])
+        maximum = max(lightcurve[i:i + min_size])
         best_badness = float("inf")
-        best_partition = []
-        for new_x in range(x + min_size, min(len(lightcurve) - 1, x + max_length + 1)):
-            height = maximum - minimum
-            new_badness = (new_x - x) * height / barmap[new_x - x]
-            compartmentalization = compartmentalize(lightcurve,
-                                                    new_x,
-                                                    memo,
-                                                    max_length,
-                                                    barmap,
-                                                    max_pval,
-                                                    min_size)
-            memo = compartmentalization[2]
-            found_badness = compartmentalization[1]
-            badness = new_badness + found_badness
-            if badness < best_badness:
-                best_badness = badness
-                best_partition = [x] + compartmentalization[0]
-            if lightcurve[new_x] < minimum:
-                minimum = lightcurve[new_x]
-            if lightcurve[new_x] > maximum:
-                maximum = lightcurve[new_x]
-        if len(lightcurve) - x <= max_length:
-            maximum = max(maximum, lightcurve[-1])
-            minimum = min(minimum, lightcurve[-1])
-            height = maximum - minimum
-            new_badness = (len(lightcurve) - x) * height / barmap[len(lightcurve) - x]
-            if new_badness < best_badness:
-                best_badness = new_badness
-                best_partition = [x, len(lightcurve)]
-        memo[x] = (best_partition, best_badness)
-        return (best_partition, best_badness, memo)
+        best_partitioning = []
+        for j in range(i + min_size, min(l + 1, i + max_length + 1)):
+            new_badness = (j - i) * (maximum - minimum) / barmap[j - i]
+            found_partitioning, found_badness = memovalues[j]
+            if new_badness + found_badness < best_badness:
+                best_badness = new_badness + found_badness
+                best_partitioning = [i] + found_partitioning
+            if j < l and lightcurve[j] > maximum:
+                maximum = lightcurve[j]
+            elif j < l and lightcurve[j] < minimum:
+                minimum = lightcurve[j]
+        memovalues[i] = (best_partitioning, best_badness)
+        i -= 1
+    return memovalues[0]
