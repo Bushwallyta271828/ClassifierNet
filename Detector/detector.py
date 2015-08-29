0c71fc2172bb407acb23f01481b8451311e37b49
Bushwallyta271828 <bushwallyta@gmail.com>
2015-08-29 16:13:50 +0100

    added the detect function - not implemented yet

--- Detector/detector.py
+++ Detector/detector.py
@@ -9,3 +9,12 @@ sys.path.insert(0, "../Classifier")
 from keywordclasses import *
 from compartmentalize import *
 
+def detect(lightcurves):
+    """
+    Author: Xander
+    This function hunts for exoplanet
+    transits in the lightcurves provided.
+    All frames must be taken at equal
+    intervals of time.
+    """
+    
