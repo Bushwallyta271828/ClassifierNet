db2a94838cf226f91ee8b7d1d59df2ec102e2a07
Bushwallyta271828 <bushwallyta@gmail.com>
2015-08-30 11:49:50 +0100

    Changed detector some more

--- Detector/detector.py
+++ Detector/detector.py
@@ -5,7 +5,6 @@ from numpy import *
 from pybrain.tools.customxml.networkreader import NetworkReader
 import sys
 sys.path.insert(0, "..")
-from keywordclasses import *
 from compartmentalize import *
 from read_classifier import *
 
@@ -20,7 +19,7 @@ def detect(lightcurves):
     the number of lightcurves to be input 
     in the neural net.
     """
-    message_file = open("../Classifier/network_info.txt")
+    message_file = open("detect_info.txt")
     lines = message_file.readlines()
     message_file.close()
     transit_index = lines.index("Transit object:")
@@ -35,12 +34,24 @@ def detect(lightcurves):
         partitionings = [compartmentalize(lightcurve, max_length=step)[0] for lightcurve in lightcurves]
     else:
         partitionings = [range(0, step * points, bin_size) + [step * points] for lightcurve in lightcurves]
-    heights = [[(max(lightcurve[partitionings[i]: partitionings[i + 1]])
-               - min(lightcurve[partitionings[i]: partitionings[i + 1]])) for i in range(len(partitionings - 1))]
-                                                                          for lightcurve in lightcurves]
+    maxs = [[max(lightcurve[partitionings[i]: partitionings[i + 1]]) for i in range(len(partitionings - 1))]
+                                                                     for lightcurve in lightcurves]
+    mins = [[min(lightcurve[partitionings[i]: partitionings[i + 1]]) for i in range(len(partitionings - 1))]
+                                                                     for lightcurve in lightcurves]
     classifications = []
-    for i in range(len(lightcurves[0]) - step * (points - 1) - 1):
+    for i in range(len(lightcurves[0]) - step * (points - 1)): #Check
         inpt = []
         for o in range(len(lightcurves)):
             for num, point in enumerate(partitionings[o][:-1]):
-                
+                next_point = partitionings[o][num + 1]
+                if ((next_point - 1 - i) // step > (point - 1 - i) // step
+                and (point - i) <= (points - 1)*steps): #Check
+                    if (point - i) % transit.generate_step > 0:
+                        start = transit.generate_step * ((point - i) // transit.generate_step + 1) + i
+                    else:  # (point - i) % transit.generate_step == 0
+                        start = point
+                    for i in range(start, next_point, transit.generate_step):
+                        inpt += [maxs[o][num], mins[o][num]]
+        classification = read_classifier(inpt)
+        classifications.append(classification)
+    
